15bf4f9061c0dd8719309aeff2113a3978948d63
ykongr <rd23052q@st.omu.ac.jp>
2026-06-24 10:58:04 +0900

    correcsion

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
-import prisma from '@/lib/prisma';
+// import prisma from '@/lib/prisma';
 import { verifyPassword, generateToken } from '@/lib/auth';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest) {
   try {
